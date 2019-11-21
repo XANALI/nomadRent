cc1839424e531c9e4706d2cb597843fb1532cab5
Miouy <57190815+Miouy@users.noreply.github.com>
2019-11-21 18:51:14 +0600

    made some changes

--- ourApp/static/ourApp/js/main.js
+++ ourApp/static/ourApp/js/main.js
@@ -162,16 +162,16 @@
             animationDuration: 20000,
             slides: [
                 {
-                    src: 'img/slider-img/slider-img-1.jpg'
+                    src: "../img/slider-img/slider-img-1.jpg"
                 },
                 {
-                    src: 'img/slider-img/slider-img-2.jpg'
+                    src: '../img/slider-img/slider-img-2.jpg'
                 },
                 {
-                    src: 'img/slider-img/slider-img-3.jpg'
+                    src: '../img/slider-img/slider-img-3.jpg'
                 },
                 {
-                    src: 'img/slider-img/slider-img-4.jpg'
+                    src: '../img/slider-img/slider-img-4.jpg'
                 }
             ]
         }); //Home Page Two Slideshow
