a48811e67f38693ab24f8cd0fab4a00a174d9a31
XANALI <53555116+XANALI@users.noreply.github.com>
2019-11-28 18:48:43 +0600

    profile page completely finished(i hope)

--- ourApp/views.py
+++ ourApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Car, ModelOfCar
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -43,7 +43,17 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'ourApp/profile.html')
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'ourApp/profile.html', {'user_form':form})
 
 def order(request):
     return render(request,'ourApp/order.html')
