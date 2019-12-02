d1e82c77e090dc9718269b2cab05e919e4cc12ad
Miouy <57190815+Miouy@users.noreply.github.com>
2019-12-02 23:43:15 +0600

    order page smth

--- ourApp/views.py
+++ ourApp/views.py
@@ -81,5 +81,30 @@ def profile(request):
 
     return render(request, 'ourApp/profile.html', {'user_form':form})
 
+
 def order(request):
-    return render(request,'ourApp/order.html')
+    cars=Car.objects.all()
+    paginator=Paginator(cars,5)
+    page_number=request.GET.get('page',1)
+    page=paginator.get_page(page_number)
+
+    is_paginated=page.has_other_pages()
+
+    if page.has_previous():
+        prev_url='?page={}'.format(page.previous_page_number())
+    else:
+        prev_url=''
+
+    if page.has_next():
+        next_url='?page={}'.format(page.next_page_number())
+    else:
+        next_url=''
+
+    context={
+        'page_object':page,
+        'is_paginated':is_paginated,
+        'next_url':next_url,
+        'prev_url':prev_url
+    }
+
+    return render(request,'ourApp/order.html', context=context)
